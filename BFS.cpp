2da8f6fad35854d77ae5cc7ea6c46418421c9810
Marko Milovanov <46499482+marmil03@users.noreply.github.com>
2021-03-30 23:25:19 +0200

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,19 @@
+const int N=150000;
+vector<int>g[N];
+int vis[N];
+
+void BFS(int root){
+  deque<int>q;
+  vis[root]=1;
+  q.push_back(root);
+  while(!q.empty()){
+    int v=q.front();
+    q.pop_front();
+    for(auto u:g[v]){
+      if(!vis[u]) {
+        q.push_back(u);
+        vis[u]=1;
+      }
+    }
+  }
+}
