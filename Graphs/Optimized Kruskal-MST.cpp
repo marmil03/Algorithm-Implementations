1739565e1acd74ccbe38cf60462510faf58589d9
Marko Milovanov <46499482+marmil03@users.noreply.github.com>
2021-05-25 18:40:51 +0200

    Update Optimized Kruskal-MST.cpp

--- Graphs/Optimized Kruskal-MST.cpp
+++ Graphs/Optimized Kruskal-MST.cpp
@@ -15,9 +15,9 @@ void union_sets(int a,int b){
   a=find_set(a);
   b=find_set(b);
   if(a!=b){
-    if(rank[a]<rank[b]) swap(a,b);
+    if(size[a]<size[b]) swap(a,b);
     parent[b]=a;
-    if(rank[a]==rank[b]) rank[a]++;
+    if(size[a]==size[b]) rank[a]++;
   }
 }
 
